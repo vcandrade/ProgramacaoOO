4ca819216d50575a2167ceab146b94eba71e45db
Vinicius Camargo Andrade <vcandrade@utfpr.edu.br>
2024-03-14 15:45:11 -0300

    added class 03

---
+++ src/aula03/Carro.java
@@ -0,0 +1,28 @@
+package aula03;
+
+public class Carro {
+
+	// atributos da classe
+	String modelo;
+	String marca;
+	int ano;
+	String placa;
+	String cor;
+
+	// métodos da classe
+	public void ligar() {
+
+	}
+
+	public void desligar() {
+
+	}
+
+	public void acelerar() {
+
+	}
+
+	public void frear() {
+
+	}
+}
---
+++ src/aula03/CarroTeste.java
@@ -0,0 +1,39 @@
+package aula03;
+
+public class CarroTeste {
+
+	public static void main(String[] args) {
+
+		// instanciar objetos
+		Carro carro1 = new Carro();
+		Carro carro2 = new Carro();
+
+		// atribuir valores aos atributos de cada objeto
+		carro1.modelo = "Gol";
+		carro1.marca = "Volkswagen";
+		carro1.ano = 2015;
+		carro1.placa = "ABC-1234";
+		carro1.cor = "Amarelo";
+
+		carro2.modelo = "Onix";
+		carro2.marca = "Chevrolet";
+		carro2.ano = 2024;
+		carro2.placa = "MNO-9876";
+		carro2.cor = "Preto";
+
+		// obter valores dos atributos do objeto
+		System.out.println("========= Carro 1 =========");
+		System.out.println("Modelo: " + carro1.modelo);
+		System.out.println("Marca: " + carro1.marca);
+		System.out.println("Ano: " + carro1.ano);
+		System.out.println("Placa: " + carro1.placa);
+		System.out.println("Cor: " + carro1.cor);
+
+		System.out.println("========= Carro 2 =========");
+		System.out.println("Modelo: " + carro2.modelo);
+		System.out.println("Marca: " + carro2.marca);
+		System.out.println("Ano: " + carro2.ano);
+		System.out.println("Placa: " + carro2.placa);
+		System.out.println("Cor: " + carro2.cor);
+	}
+}

01e2d9a686a3b040ffde473240e1a7a0c1b6b828
Vinicius Camargo Andrade <vinicius.andrade1990@gmail.com>
2024-10-02 12:07:26 -0300

    added classes 01 and 02

---
+++ src/aula01/SaidaDados.java
@@ -0,0 +1,15 @@
+package aula01;
+
+public class SaidaDados {
+
+	public static void main(String[] args) {
+		
+		String nome = "João da Silva";
+		int idade = 30;
+		double peso = 75.9;
+		
+		System.out.println(nome + " possui " + idade + " anos e " + peso + "kg.");
+		System.out.printf("%s possui %d anos e %f kg.\n", nome, idade, peso);
+		System.out.println("Disciplina: Programação Orientada a Objetos");
+	}
+}

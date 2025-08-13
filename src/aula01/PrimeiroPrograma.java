e274ee5940fdb88266685cc34e242b42072ba4b5
Vinicius Camargo Andrade <vinicius.andrade1990@gmail.com>
2025-08-12 22:24:42 -0300

    added class 01

---
+++ src/aula01/PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+package aula01;
+
+public class PrimeiroPrograma {
+
+	public static void main(String [] args) {
+	
+		System.out.println("Olá Mundo.");
+	}
+}
---
+++ src/aula01/SaidaDados.java
@@ -0,0 +1,17 @@
+package aula01;
+
+public class SaidaDados {
+
+	public static void main(String [] args) {
+		
+		String nome = "João da Silva";
+		int idade = 30;
+		double peso = 78.9;
+		
+		System.out.print(nome + " possui " + idade + " anos e " + String.format("%.2f", peso) + "kg.");
+		
+		System.out.println(nome + " possui " + idade + " anos e " + String.format("%.2f", peso) + "kg.");
+		
+		System.out.printf("%s possui %d anos e %.2f kg.\n", nome, idade, peso);
+	}
+}

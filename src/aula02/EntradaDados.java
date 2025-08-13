8c87a788b35999786c4620ec8c99d7efaac0f551
Vinicius Camargo Andrade <vinicius.andrade1990@gmail.com>
2025-08-13 12:17:37 -0300

    added class 02

---
+++ src/aula02/EntradaDados.java
@@ -0,0 +1,26 @@
+package aula02;
+
+import java.util.Scanner;
+
+public class EntradaDados {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Informe sua idade:");
+		int idade = input.nextInt();
+		
+		input.nextLine(); // limpeza buffer
+		
+		System.out.println("Informe seu nome:");
+		String nome = input.nextLine();
+		
+		System.out.println("Informe seu peso:");
+		double peso = input.nextDouble();
+		
+		System.out.println(nome + " possui " + idade + " anos e " + peso + "kg.");
+		
+		input.close();
+	}
+}

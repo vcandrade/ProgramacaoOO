b4c48d98954cb82f1c139f5c5a6f54fca57a63d5
Vinicius Camargo Andrade <vinicius.andrade1990@gmail.com>
2025-10-28 11:40:45 -0300

    added class 15

---
+++ src/aula15/heranca/VeiculoTeste.java
@@ -0,0 +1,15 @@
+package aula15.heranca;
+
+public class VeiculoTeste {
+
+	public static void main(String[] args) {
+
+		Carro carro1 = new Carro("Gol", "Volkswagen", 2020, 5, "Flex", 5);
+		carro1.calibrarPneus();
+		carro1.abastecer();
+
+		Aviao aviao1 = new Aviao("A380", "AirBus", 1980, 800, "Combustível de Aviação", 45000);
+		aviao1.voar();
+		aviao1.abastecer();
+	}
+}

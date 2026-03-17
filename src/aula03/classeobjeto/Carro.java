0ea4b593474372d2b2630813b1001d904be19d99
Vinicius Camargo Andrade <vcandrade@utfpr.edu.br>
2026-03-17 12:07:00 -0300

    added class 03

---
+++ src/aula03/classeobjeto/Carro.java
@@ -0,0 +1,28 @@
+package aula03.classeobjeto;
+
+public class Carro {
+
+	// atributos da classe Carro
+	String modelo;
+	String marca;
+	int ano;
+	String placa;
+	String cor;
+
+	// métodos da classe Carro
+	public void ligar() {
+
+	}
+
+	public void desligar() {
+
+	}
+
+	public void acelerar() {
+
+	}
+
+	public void frear() {
+
+	}
+}
---
+++ src/aula03/classeobjeto/CarroTeste.java
@@ -0,0 +1,67 @@
+package aula03.classeobjeto;
+
+import java.util.Scanner;
+
+public class CarroTeste {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		// instanciação de objetos da classe Carro
+		Carro carro1 = new Carro();
+		Carro carro2 = new Carro();
+		Carro carro3 = new Carro();
+
+		// definição dos valores para cada objeto
+		carro1.modelo = "458 Italia";
+		carro1.marca = "Ferrari";
+		carro1.ano = 2015;
+		carro1.placa = "ABC-1234";
+		carro1.cor = "Vermelho";
+
+		carro2.modelo = "Civic";
+		carro2.marca = "Honda";
+		carro2.ano = 2020;
+		carro2.placa = "MNO-9876";
+		carro2.cor = "Azul";
+
+		System.out.println("Informe o modelo:");
+		carro3.modelo = input.nextLine();
+		System.out.println("Informe a marca:");
+		carro3.marca = input.nextLine();
+		System.out.println("Informe o ano:");
+		carro3.ano = input.nextInt();
+		input.nextLine(); // limpeza de buffer
+		System.out.println("Informe a placa:");
+		carro3.placa = input.nextLine();
+		System.out.println("Informe a cor:");
+		carro3.cor = input.nextLine();
+
+		// impressão dos dados de cada objeto
+		System.out.println(">>>>>> CARRO 1 <<<<<<");
+		System.out.println("Modelo: " + carro1.modelo);
+		System.out.println("Marca: " + carro1.marca);
+		System.out.println("Ano: " + carro1.ano);
+		System.out.println("Placa: " + carro1.placa);
+		System.out.println("Cor: " + carro1.cor);
+
+		System.out.println("=====================");
+
+		System.out.println(">>>>>> CARRO 2 <<<<<<");
+		System.out.println("Modelo: " + carro2.modelo);
+		System.out.println("Marca: " + carro2.marca);
+		System.out.println("Ano: " + carro2.ano);
+		System.out.println("Placa: " + carro2.placa);
+		System.out.println("Cor: " + carro2.cor);
+
+		System.out.println("=====================");
+
+		System.out.println(">>>>>> CARRO 3 <<<<<<");
+		System.out.println("Modelo: " + carro3.modelo);
+		System.out.println("Marca: " + carro3.marca);
+		System.out.println("Ano: " + carro3.ano);
+		System.out.println("Placa: " + carro3.placa);
+		System.out.println("Cor: " + carro3.cor);
+	}
+}
